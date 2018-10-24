63d00c5b88873661a7f6aea779fc731a40eacbd2
Markus Cozowicz <eisber@eisber.net>
2018-10-24 18:10:09 +0200

    initial commit of sarplus caching

---
+++ scala/build.sbt
@@ -0,0 +1,14 @@
+name := "sarplus"
+
+organization := "eisber"
+
+scalaVersion := "2.11.8"
+
+val sparkVersion = "2.3.0"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "commons-io" % "commons-io" % "2.6"
+  "com.google.guava" % "guava" % "25.0-jre"
+)
---
+++ scala/src/main/scala/eisber/sarplus/SARCacheOutputWriter.scala
@@ -0,0 +1,98 @@
+package eisber.sarplus
+
+import java.io.{DataOutputStream, FileInputStream, FileOutputStream}
+
+import org.apache.hadoop.mapreduce.{RecordWriter, TaskAttemptContext, TaskAttemptID}
+import org.apache.hadoop.fs.{FileStatus, Path}
+import org.apache.spark.sql.catalyst.{CatalystTypeConverters, InternalRow}
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.execution.datasources.{CodecStreams, OutputWriter}
+import org.apache.spark.sql.types._
+
+import org.apache.commons.io.IOUtils
+import com.google.common.io.LittleEndianDataOutputStream
+
+class SARCacheOutputWriter(
+    path: String,
+    context: TaskAttemptContext,
+    schema: StructType) extends OutputWriter
+{
+  val pathOffset = path + ".offsets"
+  val pathRelated = path + ".related"
+
+  val outputOffset = new LittleEndianDataOutputStream(new FileOutputStream(pathOffset))
+  val outputRelated = new LittleEndianDataOutputStream(new FileOutputStream(pathRelated))
+
+  // On databricks the above 2 files aren't copied
+  val outputFinal = new LittleEndianDataOutputStream(CodecStreams.createOutputStream(context, new Path(path)))
+
+  var lastId = Long.MaxValue
+
+  var rowNumber = 0L
+  var offsetCount = 0L
+
+  // TODO: validate schema
+
+  // this is the new api in spark 2.2+
+  def write(row: InternalRow): Unit = {
+    // i1, i2, value
+    val i1 = row.getLong(0)
+    val i2 = row.getLong(1)
+    val value = row.getDouble(2)
+
+    // System.out.println("i1:  "+ i1 + " i2: " + i2 +  " Value:  " + value) // debug
+    if(lastId != i1)
+    {
+        // System.out.println("RowNumber: " + rowNumber) // debug
+        outputOffset.writeLong(rowNumber) 
+        offsetCount += 1
+        lastId = i1
+    }
+
+    outputRelated.writeInt(i2.toInt)
+    outputRelated.writeFloat(value.toFloat)
+
+    rowNumber += 1
+  }
+
+  // api in spark 2.0 - 2.1
+  def write(row: Row): Unit = {
+    // i1, i2, value
+    val i1 = row.getLong(0)
+    val i2 = row.getLong(1)
+    val value = row.getDouble(2)
+
+    if(lastId != i1)
+    {
+        outputOffset.writeLong(rowNumber) 
+        offsetCount += 1
+        lastId = i1
+    }
+
+    outputRelated.writeInt(i2.toInt)
+    outputRelated.writeFloat(value.toFloat)
+
+    rowNumber += 1
+  }
+
+  override def close(): Unit = 
+  {
+      outputOffset.writeLong(rowNumber)
+      offsetCount += 1
+
+      outputOffset.close
+      outputRelated.close
+
+      outputFinal.writeLong(offsetCount)
+
+      var input = new FileInputStream(pathOffset)
+      IOUtils.copy(input, outputFinal)
+      input.close
+
+      input = new FileInputStream(pathRelated)
+      IOUtils.copy(input, outputFinal)
+      input.close
+
+      outputFinal.close
+  } 
+}
